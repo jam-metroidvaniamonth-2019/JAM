6a8286f1f93e6a3972a3d13e07367cd9981337f2
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2019-08-20 00:18:15 -0600

    Added Dynamic Target Camera for Player

---
+++ Assets/Scripts/Camera/CameraTargetMarker.cs
@@ -0,0 +1,39 @@
+﻿using Cinemachine;
+using UnityEngine;
+using Utils;
+
+namespace CustomCamera
+{
+    public class CameraTargetMarker : MonoBehaviour
+    {
+        [SerializeField] private float _minActivationDistance;
+        [SerializeField] [Range(0, 10)] private float _targetPriority;
+        [SerializeField] private float _targetRadius;
+
+        private Transform _player;
+        private CinemachineTargetGroup _targetCamera;
+
+        private bool _targetAdded;
+
+        private void Start()
+        {
+            _player = GameObject.FindGameObjectWithTag(TagManager.Player).transform;
+            _targetCamera = GameObject.FindGameObjectWithTag(TagManager.TargetCamera).GetComponent<CinemachineTargetGroup>();
+        }
+
+        private void Update()
+        {
+            if (Vector2.Distance(_player.position, transform.position) <= _minActivationDistance && !_targetAdded)
+            {
+                _targetAdded = true;
+                _targetCamera.AddMember(transform, _targetPriority, _targetRadius);
+            }
+
+            else if (Vector2.Distance(_player.position, transform.position) > _minActivationDistance && _targetAdded)
+            {
+                _targetAdded = false;
+                _targetCamera.RemoveMember(transform);
+            }
+        }
+    }
+}
