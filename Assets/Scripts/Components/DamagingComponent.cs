337c783924742bacd3054843d0abcda2fb256936
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2019-09-02 09:52:29 -0600

    Enemy master merge (#5)
    * EnemyBaseStructure Skeleton
    
    * Enemy Waypoint Movement Sample
    
    * temp solution for stopping continuous damage
    
    * Enemy 1 - Nikhil Design Mold
    
    * Enemy Activation Point and Enemy 1 Prefab
    
    * Fixed StateChange
    
    * Enemy 4 Monitoring Range Implemented
    
    * Enemy WIP and EnemyDash with Damage and Invetiating Component

---
+++ Assets/Scripts/Components/DamagingComponent.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagingComponent : MonoBehaviour
+{
+    public Common.HealthSetter playerHealthSetter;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var _validHealthSetter = collision.GetComponent<Common.HealthSetter>();
+
+        if (_validHealthSetter)
+        {
+            if (!playerHealthSetter)
+            {
+                playerHealthSetter = _validHealthSetter;
+            }
+        }       
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        var _validHealthSetter = collision.GetComponent<Common.HealthSetter>();
+
+        if (_validHealthSetter)
+        {
+            if (playerHealthSetter)
+            {
+                playerHealthSetter = null;
+            }
+        }
+    }
+
+    public bool DealDamage(float _damageValue)
+    {
+        if (playerHealthSetter)
+        {
+            playerHealthSetter.ReduceHealth(_damageValue);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
