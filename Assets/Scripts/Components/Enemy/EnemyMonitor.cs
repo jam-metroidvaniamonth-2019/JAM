fcf05f7568fa9bd1b27a668cbb97a417715001d6
iyashna <iyashj@outlook.com>
2019-09-07 20:24:41 +0530

    Enemy Navigation Fixed

---
+++ Assets/Scripts/Components/Enemy/EnemyMonitor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMonitor : MonoBehaviour
+{
+    public Collider2D monitoringRange;
+
+
+
+}
---
+++ Assets/Scripts/Components/Enemy/EnemySkin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySkin : MonoBehaviour
+{
+    // damage on touching the enemy
+    [SerializeField]
+    private float damage;
+    [SerializeField]
+    private Collider2D enemyCollider;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var collisionScript = collision.GetComponent<Player.Movement.PlayerCollision>();
+        if (collisionScript)
+        {
+            var healthSetter =
+            collisionScript.GetComponent<Common.HealthSetter>();
+
+            if (healthSetter.bCanDealDamage)
+            {
+                healthSetter.ReduceHealth(damage);
+                healthSetter.AllowDamage();
+            }
+
+        }
+    }
+
+}
