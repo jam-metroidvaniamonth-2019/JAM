f3574849dea30375ff9c1d54756d35a6e967664c
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2019-09-12 17:04:31 -0600

    Fixed Enemies Script Missing

--- Assets/Scripts/Components/Enemy/EnemySkin.cs
+++ Assets/Scripts/Components/Enemy/EnemySkin.cs
@@ -1,24 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemySkin : MonoBehaviour
 {
     // damage on touching the enemy
-    [SerializeField]
-    private float damage;
-    [SerializeField]
-    private Collider2D enemyCollider;
+    [SerializeField] private float damage;
+    [SerializeField] private Collider2D enemyCollider;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var collisionScript = collision.GetComponent<Player.Movement.PlayerCollision>();
         if (collisionScript)
         {
-            var healthSetter =
-            collisionScript.GetComponent<Common.HealthSetter>();
-            healthSetter.DealInstantaneousDamage(damage, 1f);
+            var healthSetter = collisionScript.GetComponent<Common.HealthSetter>();
+            healthSetter.ReduceHealth(damage);
         }
     }
-
 }
