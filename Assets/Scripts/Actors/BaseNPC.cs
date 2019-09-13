3960078aa8c1beafab00de123f1db5777fde183d
iyashna <iyashj@outlook.com>
2019-09-14 04:54:44 +0530

    ENemyHealthSetter on flyinh_charge_enemy

--- Assets/Scripts/Actors/BaseNPC.cs
+++ Assets/Scripts/Actors/BaseNPC.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class BaseNPC : MonoBehaviour
 {
+    public EnemyHealthObject enemyHealthObject;
+
+    public virtual void Start()
+    {
+        enemyHealthObject.enemyHealthSetter.OnHealthZero += EnemyDead;
+    }
+
+    private void EnemyDead()
+    {
+        // this character is dead
+    }
+
     public float detectionDelay;
     public float damageOnTouch;
     public float speed;
 
     public virtual void Init()
     {
-
+        
     }
 
     public void DealDamage(ref Common.HealthSetter healthSetter, float _damage)
