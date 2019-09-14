f35a1551f25c2444c37173bf836d49307dfea39d
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2019-09-14 09:38:04 -0600

    Added Dialogue for Enemy Speech

--- Assets/Scripts/Scenes/Main/Enemy/EnemyDeathBagDrop.cs
+++ Assets/Scripts/Scenes/Main/Enemy/EnemyDeathBagDrop.cs
@@ -1,4 +1,5 @@
 using System;
+using SpeechSystem;
 using UnityEngine;
 
 namespace Scenes.Main.Enemy
@@ -6,11 +7,14 @@ namespace Scenes.Main.Enemy
     public class EnemyDeathBagDrop : EnemyDeathController
     {
         [SerializeField] private Rigidbody2D _bag;
+        [SerializeField] [TextArea] private string[] _dialogues;
 
         protected override void RunEffectOnZero()
         {
             _bag.isKinematic = false;
             _bag.transform.SetParent(null);
+
+            SimpleSpeechController.Instance.DisplayDialogues(_dialogues);
 
             base.RunEffectOnZero();
         }
